8a5533f0b11faeba71bb60e7b47def69204f3fba
liyongming <2207265761@qq.com>
2017-10-21 09:32:22 +0800

    zookeeper   time超时

--- src/com/cn/liyongming/cli/zkClient.java
+++ src/com/cn/liyongming/cli/zkClient.java
@@ -13,8 +13,8 @@ import org.junit.Test;
 public class zkClient {
 	ZooKeeper zk;
 	public static String connectString = "192.168.120.3:2181";
-	public static int sessionTimeout = 2000;
-	private CountDownLatch connectedSignal = new CountDownLatch(1);  
+	public static int sessionTimeout = 20000;
+	//private CountDownLatch connectedSignal = new CountDownLatch(1);  
 
 	public zkClient() {
 
@@ -27,24 +27,14 @@ public class zkClient {
 			};
 		});
 		System.out.println(zk.toString());
-		connectedSignal.await();  
+		//connectedSignal.await();  
 
-	}
-	@Test
-	public void zkNew() throws Exception{
-		zk =new ZooKeeper(connectString, sessionTimeout, new Watcher(){
-			public void process(WatchedEvent event){
-				System.out.println(event.getPath()+"---"+event.getType());
-			};
-		});
-		
-		System.out.println(zk.toString());
 	}
 	@Test
 	public void zkCreate() throws Exception{
 //		String cr = zk.create("/20171021", "liyongming".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-		String cr = zk.create("/liyongming", "liyongming".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-		String data = zk.getData("/liyongming", true, null).toString();
+		String cr = zk.create("/liyongming03", "liyongming".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		String data = zk.getData("/liyongming03", true, null).toString();
 		System.out.println(data);
 	}
 }
